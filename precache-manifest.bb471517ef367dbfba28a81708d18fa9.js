06ba80ab32b07e16412ec14706862e822116650e
Carlos Santino Zaracho Simonetto <carlosszaracho@gmail.com>
2020-09-01 19:25:50 -0300

    Updates

---
+++ precache-manifest.bb471517ef367dbfba28a81708d18fa9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "740550a11a600a91bef3cfe1197346bf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fd3111e7829c65eb700a",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c361b48400d8969d3aec",
+    "url": "/static/js/2.6e0af07e.chunk.js"
+  },
+  {
+    "revision": "7baac2e68c562348b516173f26c107b7",
+    "url": "/static/js/2.6e0af07e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd3111e7829c65eb700a",
+    "url": "/static/js/main.770dc545.chunk.js"
+  },
+  {
+    "revision": "113398430c874bd6fd3d",
+    "url": "/static/js/runtime-main.aa29b3d4.js"
+  }
+]);
